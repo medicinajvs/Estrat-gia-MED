96d5901eb8fee594767872043888adeaa816bd1e
medicinajvs <medicinajvs@gmail.com>
2026-01-07 16:46:39 -0300

    Descreva aqui o que você mudou

--- meu-lms/vite.config.js
+++ meu-lms/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// SUBSTITUA 'nome-do-seu-repositorio' PELO NOME QUE ESTÁ NO GITHUB
 export default defineConfig({
   plugins: [react()],
+  base: '/Estrat-gia-MED/', 
 })
