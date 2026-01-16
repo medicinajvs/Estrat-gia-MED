f32f384810022a85ec739b0c7505b684c15e8716
medicinajvs <medicinajvs@gmail.com>
2026-01-16 00:44:51 -0300

    Delete meu-lms directory

--- meu-lms/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// SUBSTITUA 'nome-do-seu-repositorio' PELO NOME QUE ESTÁ NO GITHUB
-export default defineConfig({
-  plugins: [react()],
-  base: '/Estrat-gia-MED/', 
-})
